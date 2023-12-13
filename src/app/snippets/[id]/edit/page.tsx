3631beed239364915270231cc576eac56665c1a4
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-13 21:07:21 +0200

    Client Component

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,3 +1,6 @@
+import SnippetEditForm from '@/components/SnippetEditForm';
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 interface SnippetEditPageProps{
@@ -6,9 +9,20 @@ interface SnippetEditPageProps{
     }
 }
 
-export default function SnippetEditPage(props: SnippetEditPageProps) {
-    const id = parseInt(props.params.id)
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+    const id = parseInt(props.params.id);
+    const snippet = await db.snippet.findFirst({where: {id}});
+
+    if(!snippet){
+        return notFound();
+    }
+
+
+
     return (
-    <div>Editing {id}</div>
-  )
+      <div>
+        Editing title: <span className='font-bold'>{snippet.title}</span>
+        <SnippetEditForm snippet={snippet}/>
+      </div>
+    );
 }
