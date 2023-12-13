7ca3e8d811a7c37fef4bb8e24208e4bf5525eb53
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-13 18:37:45 +0200

    Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   return (
-    <div>{snippets.map((snippet) => <div key={snippet.id}>{snippet.title}</div>)}</div>
+    <div>
+      <h1>Home</h1>
+      <div className="flex justify-between items-center mb-2">
+        <h2>Snippets</h2>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-4">
+        {snippets.map((snippet) => (
+          <Link
+            href={`/snippets/${snippet.id}`}
+            key={snippet.id}
+            className="flex justify-between items-center p-2 border rounded"
+          >
+            {snippet.title}
+            <div>View</div>
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 }
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,5 @@
 import { db } from "@/db";
-import { notFound, redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default function SnippetsCreatePage() {
