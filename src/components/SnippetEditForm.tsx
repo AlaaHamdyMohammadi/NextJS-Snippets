5525dd688e4730a40b585b840226e8629793d751
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-13 23:31:49 +0200

    Separate Actions

--- src/components/SnippetEditForm.tsx
+++ src/components/SnippetEditForm.tsx
@@ -1,21 +1,27 @@
-'use client';
+"use client";
 
-import React from 'react'
-import { Snippet } from '@prisma/client';
-import { Editor } from '@monaco-editor/react';
+import React, { useState } from "react";
+import { Snippet } from "@prisma/client";
+import { Editor } from "@monaco-editor/react";
 
-interface SnippetEditFormProps{
-    snippet: Snippet
+interface SnippetEditFormProps {
+  snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+  function handleEditorChange(value: string = "") {
+    setCode(value);
+    console.log(value);
+  }
   return (
     <Editor
       height="40vh"
       theme="vs-dark"
       language="javascript"
       defaultValue={snippet.code}
-      options={{minimap: {enabled: false}}}
+      options={{ minimap: { enabled: false } }}
+      onChange={handleEditorChange}
     />
   );
 }
