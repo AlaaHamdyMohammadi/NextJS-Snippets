6e8f5ed528652874973204c065b707f65864a963
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-14 00:07:58 +0200

    Update Data

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,19 @@
 'use server';
 
-export  async function editSnippet(){
-    console.log('Edit Snippet');
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+/*
+Need two arguments: 
+    1- id of this snippet
+    2- code to be edit it
+*/
+export  async function editSnippet(id: number, code: string){
+    // console.log(id, code);
+    await db.snippet.update({
+        where: {id},
+        data: {code},
+    })
+
+    redirect(`/snippets/${id}`);
 }
--- src/components/SnippetEditForm.tsx
+++ src/components/SnippetEditForm.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { Snippet } from "@prisma/client";
 import { Editor } from "@monaco-editor/react";
+import * as actions from "@/actions";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -14,14 +15,24 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
     setCode(value);
     console.log(value);
   }
+
+  // bind make a preloaded version of our server action, that's going to be called with some arguments already assigned to it
+  //first arg always be null, second arg must be first arg in editSnippet func in action file(id), third is the second in action
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code); // code is piece of state
   return (
-    <Editor
-      height="40vh"
-      theme="vs-dark"
-      language="javascript"
-      defaultValue={snippet.code}
-      options={{ minimap: { enabled: false } }}
-      onChange={handleEditorChange}
-    />
+    <>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">Save</button>
+      </form>
+    </>
   );
 }
