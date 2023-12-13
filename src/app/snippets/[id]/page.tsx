353111e5c70b9653ff306fefe67fd965144fdc4f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-13 20:29:44 +0200

    Show Page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,18 +1,29 @@
-import { db } from '@/db'
-import { notFound } from 'next/navigation'
-import React from 'react'
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import React from "react";
 
-export default async function SnippetShowPage({params}) {
-    console.log(params)
-    const snippet = await db.snippet.findFirst({
-        where: {
-            id: parseInt(params.id)
-        }
-    })
-    if(!snippet){
-        return notFound()
-    }
+export default async function SnippetShowPage({ params }) {
+  console.log(params);
+  const snippet = await db.snippet.findFirst({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!snippet) {
+    return notFound();
+  }
   return (
-    <div>Show a snippet: {snippet.title}</div>
-  )
+    <>
+    <div className="flex m-4 justify-between items-center">
+      <h1 className="text-xl font-bold">Show a snippet: {snippet.title}</h1>
+        <div className="flex gap-4">
+            <button className="p-2 border rounded">Edit</button>
+            <button className="p-2 border rounded">Delete</button>
+        </div>
+    </div>
+    <pre className="p-3 rounded border bg-slate-200 border-slate-200">
+        <code>{snippet.code}</code>
+    </pre>
+    </>
+  );
 }
