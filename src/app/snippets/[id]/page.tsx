62950a3ecab859bca6b561770e871aa4acc66f33
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-14 01:47:30 +0200

    Delete Data

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,30 +1,47 @@
+import React from "react";
+import * as actions from "@/actions";
 import { db } from "@/db";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import React from "react";
 
-export default async function SnippetShowPage({ params }) {
-  console.log(params);
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  //   console.log(params);
   const snippet = await db.snippet.findFirst({
     where: {
-      id: parseInt(params.id),
+      id: parseInt(props.params.id),
     },
   });
   if (!snippet) {
     return notFound();
   }
+
+  const deleteSnippetAcion = actions.deleteSnippet.bind(null, snippet.id);
+
   return (
     <>
-    <div className="flex m-4 justify-between items-center">
-      <h1 className="text-xl font-bold">Show a snippet: {snippet.title}</h1>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">Show a snippet: {snippet.title}</h1>
         <div className="flex gap-4">
-            <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          <form action={deleteSnippetAcion}>
             <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
-    </div>
-    <pre className="p-3 rounded border bg-slate-200 border-slate-200">
+      </div>
+      <pre className="p-3 rounded border bg-slate-200 border-slate-200">
         <code>{snippet.code}</code>
-    </pre>
+      </pre>
     </>
   );
 }
